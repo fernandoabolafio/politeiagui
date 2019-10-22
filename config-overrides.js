baaf3a8a65515d233da38ccecc40da7d92fe5a86
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-10-22 21:54:28 +0200

    modify config-override

--- config-overrides.js
+++ config-overrides.js
@@ -1,45 +1,27 @@
 const path = require("path");
-const { override, addWebpackAlias } = require("customize-cra");
-const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 
-const cspConfigPolicy = {
-  "default-src": "'none'",
-  "base-uri": "'self'",
-  "object-src": "'none'",
-  "script-src": ["'self'"],
-  "style-src": [
-    "'self'",
-    "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='"
-  ],
-  "connect-src":
-    "'self' faucet.decred.org explorer.dcrdata.org testnet.dcrdata.org testnet.decred.org mainnet.decred.org",
-  "manifest-src": "'self'",
-  "img-src": "'self' data:",
-  "font-src": "'self' data:"
-};
-
-module.exports = function override(config) {
-  config.resolve = {
-    ...config.resolve,
-    alias: { src: path.resolve(__dirname, "src") }
-  };
-
-  return config;
-};
-
-function addCspHtmlWebpackPlugin(config) {
+function addScriptAttr(config) {
   if (process.env.NODE_ENV === "production") {
-    config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
+    config.plugins.push(
+      new ScriptExtHtmlWebpackPlugin({
+        custom: {
+          test: /\.js$/,
+          attribute: "nonce",
+          value: "{{ cspNonce }}"
+        }
+      })
+    );
   }
-
-  return config;
 }
 
 module.exports = {
-  webpack: override(
-    addCspHtmlWebpackPlugin,
-    addWebpackAlias({
-      src: path.resolve(__dirname, "src")
-    })
-  )
+  webpack: function(config) {
+    addScriptAttr(config);
+    config.resolve = {
+      ...config.resolve,
+      alias: { src: path.resolve(__dirname, "src") }
+    };
+    return config;
+  }
 };
